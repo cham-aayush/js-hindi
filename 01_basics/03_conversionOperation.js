cfe0b9368d4d414de231330fd8fceed5ee1c36b1
cham-aayush <120579452+cham-aayush@users.noreply.github.com>
2024-08-21 13:23:39 +0000

    5th

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,9 +1,26 @@
 let score = 33
-console.log(typeof score);
+//console.log(typeof score);
 
 let points = true
 
 let valueInNumber = Number(points)
 
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// ***********************OPERATIONS**************************
+
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+let str1 = "hello"
+let str2 = "aayush"
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1"+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
