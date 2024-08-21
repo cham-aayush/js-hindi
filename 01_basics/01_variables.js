d29b975fd709c5eca9ec1a2250c2f31a3023362e
cham-aayush <120579452+cham-aayush@users.noreply.github.com>
2024-08-21 12:31:21 +0000

    2nd commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 143376
+let accountEmail = "aayus12@gmail.com"
+var accountPass = "aayush2020"
+accountCity = "Jaipur"
+
+//accountId = 2
+
+accountEmail = "yyyuyu@gmail.com"
+accountPass = "pisow"
+accountCity = "Dehradun"
+console.log(accountId)
+ 
+console.table([accountId,accountEmail,accountPass,accountCity])
