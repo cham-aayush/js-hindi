c63364853fab1cbb88cb23eb6ae6fd0742ef3ff6
cham-aayush <120579452+cham-aayush@users.noreply.github.com>
2024-08-21 12:31:43 +0000

    3rd commit

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") we are using nodejs, not browser
+
+console.log(3
+    +3)// code readability should be high
+console.log("Aayush")
+
+
+let name = "Aayush"
+let age = 21
+let isLoggedIn = false 
+let state = null
+
+/*
+number=>2 to power 53
+bigint
+string =>""
+boolean => true/false
+null => standalone value
+undefined =>
+symbol => unique
+*/
+
